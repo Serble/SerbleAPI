7ea81e6e4915aa1c9ebe18afda15b250b4798f96
copokbl <zaneharrison2006@icloud.com>
2022-10-26 14:25:02 +1100

    Changed order success endpoint that may be used in the future

---
+++ API/v1/Payments/OrderSuccess.cs
@@ -0,0 +1,19 @@
+using GeneralPurposeLib;
+using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
+
+namespace SerbleAPI.API.v1.Payments; 
+
+[Route("api/v1/payments/ordersuccess")]
+[ApiController]
+public class OrderSuccess : ControllerManager {
+    
+    [HttpGet("{session_id}")]
+    public IActionResult Get(string session_id) {
+        SessionService sessionService = new();
+        Session session = sessionService.Get(session_id);
+        Logger.Debug(!session.Livemode ? "Order Success - Test Session" : "Order Success");
+        return Redirect(Program.Config!["website_url"] + $"/store/success?session_id={session_id}");
+    }
+    
+}
